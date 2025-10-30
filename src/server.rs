26e9a985cdc48cdbacf0293453e1781d32e308c2
Predä <46051820+PredaaA@users.noreply.github.com>
2025-10-30 13:09:48 +0100

    Fix format and clippy

--- src/server.rs
+++ src/server.rs
@@ -384,7 +384,10 @@ async fn handler(
     let response = match segments[..] {
         ["metrics"] => Response::builder()
             .status(StatusCode::OK)
-            .header(hyper::header::CONTENT_TYPE, "text/plain; version=0.0.4; charset=utf-8")
+            .header(
+                hyper::header::CONTENT_TYPE,
+                "text/plain; version=0.0.4; charset=utf-8",
+            )
             .body(Body::from(metrics.render()))
             .unwrap(),
         ["shard-count"] => {
